4d519c9426db6b310064fa88285f370fc8e45079
BoaduB <boansibk@mail.uc.edu>
2023-02-08 12:32:30 -0500

    LAB_05_04_THEATERKIOSK

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,14 @@
+public class TheaterKiosk {
+    public static void main(String[] args)
+    {
+        //Enter your age
+        int age = 20;
+        int REQUIRED_AGE = 21;
+
+        if (age >= REQUIRED_AGE)
+        {
+            System.out.println("You get a wrist band. Please enjoy the movie!");
+        }
+
+    }
+}
